154fb0527440344274cf290078d984726de8b81c
Andrew Simonyan <68228647+datageek06@users.noreply.github.com>
2026-07-04 13:38:52 +0400

    Add files via upload

---
+++ Final/app/src/main/java/com/recipeapp/student/firebase/Firestore.kt
@@ -0,0 +1,52 @@
+package com.recipeapp.student.repository
+
+import com.google.firebase.firestore.FirebaseFirestore
+import com.recipeapp.student.model.Chef
+import com.recipeapp.student.model.Recipe
+
+class FirebaseRepository {
+
+    private val firestore = FirebaseFirestore.getInstance()
+
+    fun getChefs(onResult: (List<Chef>) -> Unit) {
+        firestore.collection("chefs")
+            .addSnapshotListener { snapshot, error ->
+                if (error != null) return@addSnapshotListener
+                val chefsList = mutableListOf<Chef>()
+                snapshot?.let {
+                    for (document in it.documents) {
+                        val chef = document.toObject(Chef::class.java)
+                        if (chef != null) {
+                            chefsList.add(chef)
+                        }
+                    }
+                }
+                onResult(chefsList)
+            }
+    }
+
+    fun getRecipes(onResult: (List<Recipe>) -> Unit) {
+        firestore.collection("recipes")
+            .addSnapshotListener { snapshot, error ->
+                if (error != null) return@addSnapshotListener
+                val recipesList = mutableListOf<Recipe>()
+                snapshot?.let {
+                    for (document in it.documents) {
+                        val recipe = document.toObject(Recipe::class.java)
+                        if (recipe != null) {
+                            recipesList.add(recipe)
+                        }
+                    }
+                }
+                onResult(recipesList)
+            }
+    }
+
+    fun toggleFollowChef(chefId: String, currentStatus: Boolean) {
+        firestore.collection("chefs").document(chefId).update("isFollowed", !currentStatus)
+    }
+
+    fun toggleLikeRecipe(recipeId: String, currentStatus: Boolean) {
+        firestore.collection("recipes").document(recipeId).update("isLiked", !currentStatus)
+    }
+}
